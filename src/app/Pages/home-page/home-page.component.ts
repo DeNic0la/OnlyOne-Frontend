5ffe2e56b8d511ac46df0efc0f5504572dc77ff0
DeNicola <nicola@fioretti.info>
2022-10-17 22:19:44 +0200

    add unsub and remove log

--- src/app/Pages/home-page/home-page.component.ts
+++ src/app/Pages/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RoomService} from "../../service/room.service";
-import {interval, Observable, startWith, Subject, switchMap} from "rxjs";
+import {interval, Observable, startWith, Subject, Subscription, switchMap} from "rxjs";
 import {Room} from "../../service/service";
 
 @Component({
@@ -8,21 +8,28 @@ import {Room} from "../../service/service";
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, OnDestroy{
 
-  constructor(private roomService:RoomService) {
+  constructor(private roomService: RoomService) {
   }
 
 
-  public rooms:Room[] = [];
+  public rooms: Room[] = [];
+
+  private sub: Subscription|undefined;
 
   public labels:string[] = ['Room','Join','Player Count'];
 
   ngOnInit(): void {
-    interval(5000).pipe(
+    this.sub = interval(5000).pipe(
       startWith(0),
       switchMap(() => this.roomService.Rooms)
-    ).subscribe(res=> {console.log(res);this.rooms = res;})
-
+    ).subscribe(res=> {this.rooms = res;})
   }
+
+  ngOnDestroy(): void {
+    if (this.sub) this.sub.unsubscribe()
+  }
+
+
 }
