86d50a650154c74c5af4005435a1fcf19714e007
DeNicola <nicola@fioretti.info>
2022-10-19 19:42:36 +0200

    make funny service

---
+++ src/app/service/namelix.service.ts
@@ -0,0 +1,39 @@
+import {Injectable, Provider} from '@angular/core';
+
+@Injectable()
+/**
+ * This is a Singleton
+ */
+export class NamelixService {
+  private username:string;
+  private static instance:NamelixService|undefined;
+  public static getInstance():NamelixService {
+    if (NamelixService.instance){
+      return NamelixService.instance;
+    }
+    else {
+      return new NamelixService();
+    }
+  }
+  private constructor() {
+    let uName = localStorage.getItem("username");
+    if (uName && uName.length > 0) this.username = uName
+    else this.username = "";
+  }
+
+  get name():string{
+    return this.username;
+  }
+  set name(val:string) {
+    this.username = val;
+    localStorage.setItem("username",val);
+  }
+}
+export const NAMELIX_PROVIDER:Provider =
+  {
+    provide:NamelixService,
+    useFactory: (): NamelixService => {
+      return NamelixService.getInstance();
+    }
+  }
+
