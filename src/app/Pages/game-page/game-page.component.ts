81d11c7b10880c3bd9b0a68c1dd90b852e98eca6
DeNic0la <nicola@fioretti.info>
2022-11-06 16:54:01 +0100

    add card overview on Game

--- src/app/Pages/game-page/game-page.component.ts
+++ src/app/Pages/game-page/game-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CardColor, CardNumber} from "../../Components/card/card.types";
 
 @Component({
   selector: 'app-game-page',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePageComponent implements OnInit {
 
+  public cards:Card[] = [
+    {
+      number: 1,
+      color: "blue"
+    },
+    {
+      number: 2,
+      color: "green"
+    },
+    {
+      number: 3,
+      color: "red"
+    },
+    {
+      number: 4,
+      color: "yellow"
+    },
+    {
+      number: 5,
+      color: "red"
+    },
+    {
+      number: 6,
+      color: "green"
+    },
+    {
+      number: 7,
+      color: "blue"
+    },
+    {
+      number: 8,
+      color: "yellow"
+    },
+    {
+      number: 9,
+      color: "green"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+export interface Card {
+  number:CardNumber;
+  color:CardColor;
 }
